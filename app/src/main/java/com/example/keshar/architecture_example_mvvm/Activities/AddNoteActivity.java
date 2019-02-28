cf30aa40313c70913415ecaf5842599f82f26ad6
Keshar <paudelkeshar2055@gmail.com>
2019-02-28 16:32:38 +0545

    insert data in database is implemented and add note feature is implemented.

---
+++ app/src/main/java/com/example/keshar/architecture_example_mvvm/Activities/AddNoteActivity.java
@@ -0,0 +1,76 @@
+package com.example.keshar.architecture_example_mvvm.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.example.keshar.architecture_example_mvvm.R;
+
+public class AddNoteActivity extends AppCompatActivity {
+    public static final String EXTRA_TITLE=
+            "com.example.keshar.architecture_example_mvvm.Activities.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION=
+            "com.example.keshar.architecture_example_mvvm.Activities.EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY=
+            "com.example.keshar.architecture_example_mvvm.Activities.EXTRA_PRIORITY";
+    private EditText edtTextTitle;
+    private EditText edtTexrDescription;
+    private NumberPicker numberPickerPriority;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        edtTextTitle = findViewById(R.id.edt_txt_title);
+        edtTexrDescription = findViewById(R.id.edt_txt_description);
+        numberPickerPriority = findViewById(R.id.number_picker_priority);
+        numberPickerPriority.setMaxValue(1);
+        numberPickerPriority.setMaxValue(9);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Note");
+
+
+    }
+    private void saveNote(){
+        String title=edtTextTitle.getText().toString();
+        String description=edtTexrDescription.getText().toString();
+        int priority=numberPickerPriority.getValue();
+
+        if(title.trim().isEmpty() || description.trim().isEmpty()){
+            Toast.makeText(this, "Please insert Title and Description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent data=new Intent();
+        data.putExtra(EXTRA_TITLE,title);
+        data.putExtra(EXTRA_DESCRIPTION,description);
+        data.putExtra(EXTRA_PRIORITY,priority);
+        setResult(RESULT_OK,data);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
--- app/src/main/java/com/example/keshar/architecture_example_mvvm/MainActivity.java
+++ app/src/main/java/com/example/keshar/architecture_example_mvvm/MainActivity.java
@@ -2,30 +2,38 @@ package com.example.keshar.architecture_example_mvvm;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
 
+import com.example.keshar.architecture_example_mvvm.Activities.AddNoteActivity;
 import com.example.keshar.architecture_example_mvvm.Adapters.NoteAdapter;
 import com.example.keshar.architecture_example_mvvm.RoomDatabase.NoteEntity;
 import com.example.keshar.architecture_example_mvvm.ViewModel.NoteViewModel;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final int ADD_NOTE_REQUEST=1;
 
     private NoteViewModel noteViewModel;
     private RecyclerView recyclerView;
     private NoteAdapter noteAdapter;
+    private FloatingActionButton btnAddNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("ViewModelApp");
+        btnAddNote=findViewById(R.id.btn_add);
+        btnAddNote.setOnClickListener(this);
         recyclerView=findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
@@ -40,5 +48,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         recyclerView.setAdapter(noteAdapter);
+    }
+    private void addNote(){
+        Intent intent=new Intent(MainActivity.this,AddNoteActivity.class);
+        startActivityForResult(intent,ADD_NOTE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==ADD_NOTE_REQUEST && resultCode==RESULT_OK){
+            String title=data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description=data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority=data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY,1);
+            
+            NoteEntity noteEntity=new NoteEntity(title,description,priority);
+            noteViewModel.insert(noteEntity);
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show();
+        }else
+            Toast.makeText(this, "Note not Saved", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.equals(btnAddNote))
+            addNote();
+
     }
 }
